aa59051969e743d83cf65cd9edc3aea7489a3c43
Lukas Jørgensen <lpjoergensen@gmail.com>
2016-11-03 00:41:41 +0100

    Day 27 slides and exercise WIP

--- routes/slides.js
+++ routes/slides.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 
-[1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20, 23, 24, 25, 26].forEach((current, index) => {
+[1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20, 23, 24, 25, 26, 27].forEach((current, index) => {
     router.get('/day-' + current, function(req, res, next) {
         res.render('slides/day-' + current, { title: 'Day ' + current });
     });
